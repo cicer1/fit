378ce41801c56a65e6a11c9981b0136a7aef916e
Federico Cicerone <federico.cicerone@gmail.com>
2014-07-09 02:30:00 +0200

    Graphics and stuff

--- js/ajaxCall.js
+++ js/ajaxCall.js
@@ -1,37 +1,59 @@
 $(document).ready(function(){
 
   $.ajax({url:"pages/home.html",success:function(result){
-    $("#myajaxcont").html(result);
+    $("#myajaxcont").html(result).fadeIn('fast');
   }});
 
   $("#aj-home").click(function(){
     $.ajax({url:"pages/home.html",success:function(result){
-      $("#myajaxcont").html(result);
+      removeActive();
+      $("#aj-home > div").addClass("active");
+      $("#myajaxcont").css( "display", "none");
+      $("#myajaxcont").html(result).fadeIn('fast');
     }});
   });
   $("#aj-about").click(function(){
     $.ajax({url:"pages/about.html",success:function(result){
-      $("#myajaxcont").html(result);
+      removeActive();
+      $("#aj-about > div").addClass("active");
+      $("#myajaxcont").css( "display", "none");
+      $("#myajaxcont").html(result).fadeIn('fast');
     }});
   });
   $("#aj-brands").click(function(){
     $.ajax({url:"pages/brands.html",success:function(result){
-      $("#myajaxcont").html(result);
+      removeActive();
+      $("#aj-brands > div").addClass("active");
+      $("#myajaxcont").css( "display", "none");
+      $("#myajaxcont").html(result).fadeIn('fast');
     }});
   });
   $("#aj-gallery").click(function(){
     $.ajax({url:"pages/gallery.html",success:function(result){
-      $("#myajaxcont").html(result);
+      removeActive();
+      $("#aj-gallery > div").addClass("active");
+      $("#myajaxcont").css( "display", "none");
+      $("#myajaxcont").html(result).fadeIn('fast');
     }});
   });
   $("#aj-news").click(function(){
     $.ajax({url:"pages/news.html",success:function(result){
-      $("#myajaxcont").html(result);
+      removeActive();
+      $("#aj-news > div").addClass("active");
+      $("#myajaxcont").css( "display", "none");
+      $("#myajaxcont").html(result).fadeIn('fast');
     }});
   });
   $("#aj-contacts").click(function(){
     $.ajax({url:"pages/contacts.html",success:function(result){
-      $("#myajaxcont").html(result);
+      removeActive();
+      $("#aj-contacts > div").addClass("active");
+      $("#myajaxcont").css( "display", "none");
+      $("#myajaxcont").html(result).fadeIn('fast');
     }});
   });
+
+  function removeActive() {
+      $('header > div.second nav ul li div.active').removeClass("active");
+  }
 });
